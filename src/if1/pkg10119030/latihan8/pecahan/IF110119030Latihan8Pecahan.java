6bc048608be4226859e8d1bacd6ff3042e906896
RendyDermawan10119030 <rendy.10119030@mahasiswa.unikom.ac.id>
2020-10-20 22:47:37 +0700

    First commit

---
+++ src/if1/pkg10119030/latihan8/pecahan/IF110119030Latihan8Pecahan.java
@@ -0,0 +1,20 @@
+package if1.pkg10119030.latihan8.pecahan;
+/**
+ * Nama     : Rendy Dermawan
+ * Kelas    : IF-1
+ * NIM      : 10119030
+ * Deskripsi Program :Latihan Pecahan
+ */
+public class IF110119030Latihan8Pecahan {
+
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789f;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+       
+    }
+    
+}
